c5540684c5d580f1f9577673a81d586675e83eea
Moses <moseswanyonyi@gmail.com>
2017-05-20 01:30:52 +0300

    Migrated users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519213301) do
+ActiveRecord::Schema.define(version: 20170519221516) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170519213301) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.index ["customer_id"], name: "index_debts_on_customer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
