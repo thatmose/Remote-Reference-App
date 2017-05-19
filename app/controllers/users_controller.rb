f724b1745ab4c5beac3bddc79df523644734b47e
Moses <moseswanyonyi@gmail.com>
2017-05-20 01:47:14 +0300

    Added user and session views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        session[:user_id] = @user.id # auto log in
+        # redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+      else
+        flash.now[:alert] = "Log in failed..."
+        render :new
+      end
+    end
+
+    protected
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+  end
