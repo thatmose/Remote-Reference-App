a3338b7e14428db44b3a7c66bb04be6e7a6f974f
Moses <moseswanyonyi@gmail.com>
2017-05-21 00:54:38 +0300

    search attempt. Still needs work

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,36 @@ class CustomersController < ApplicationController
   def show
   end
 
+  def index
+    if params[:q].to_i == 0
+      #Not a number
+      flash[:alert] = "Only digits allowed for ID or mobile_number"
+      render "/users/index"
+    else
+    # Check if id (8 digits) or phone number (10 digits)
+      if params[:q].length == 8
+        @customer = Customer.where("id_number = #{params[:q]}")
+        successful_search?(@customer)
+      elsif params[:q].length == 10
+        @customer = Customer.where("mobile_number = #{params[:q]}")
+        successful_search?(@customer)
+      else
+        flash[:alert] = "Incorrect format for ID or mobile_number"
+        render "/users/index"
+      end
+    end
+    puts params[:q].class
+  end
+
   protected
-#TODO Implement search
+  #Check if any customers found based on query. If not found send alert to user
+  def successful_search?(result)
+    if result.empty?
+      flash[:alert] = "Customer not found"
+      render "/users/index"
+    else
+      @customer = result
+      redirect_to "/customers/index"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:new, :create, :destroy]
-  resources :customers, only: [:show]
+  resources :customers, only: [:index]
   resources :debtors, only: [:index]
   resources :users, only: [:new, :create, :index]
   root to: 'sessions#new'
